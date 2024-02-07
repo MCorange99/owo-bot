17fb510f83ce43eb4d8d14c7a12a79651fc384e0
MCorange99 <gvidasjuknevicius2@gmail.com>
2024-02-07 17:37:18 +0200

    fuck docker v2

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 git pull origin main
-sudo docker buildx build -t owo_bot .
-sudo docker run -d --name owo_bot -v ./config:/app/config owo_bot
+cargo build --release
+systemctl --user restart mcbot
